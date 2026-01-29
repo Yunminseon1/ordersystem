0a2a0ffada21e5c8227d1d98358d481620e930b0
Yunminseon1 <Yunminseon@naver.com>
2026-01-29 14:46:10 +0900

    Spring OrderSystem 프로젝트 초기 세팅

---
+++ src/main/java/com/OrderSystem/demo/OederSystemApplication.java
@@ -0,0 +1,13 @@
+package com.OrderSystem.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OederSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OederSystemApplication.class, args);
+	}
+
+}
